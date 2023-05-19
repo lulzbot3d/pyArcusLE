4b6c42025fa41bf9c2e23fe08b1d7602d301cf79
Jelle Spijker <jelle.spijker@ultimaker.com>
2023-05-19 09:00:43 +0200

    libArcus and protobuf static instead of shared
    Also modernized the recipe
    
    Contributes to CURA-10475

--- conanfile.py
+++ conanfile.py
@@ -1,14 +1,16 @@
-import os
-
 from pathlib import Path
+from os import path
 
 from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
 from conan.tools.build import check_min_cppstd
-from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
-from conan.tools.files import copy
+from conan.tools.cmake import CMakeToolchain, CMakeDeps, CMake, cmake_layout
+from conan.tools.env import VirtualBuildEnv
+from conan.tools.files import copy, AutoPackager
+from conan.tools.microsoft import check_min_vs, is_msvc, is_msvc_static_runtime
+from conan.tools.scm import Version
 
-
-required_conan_version = ">=1.52.0"
+required_conan_version = ">=1.55.0"
 
 
 class ArcusConan(ConanFile):
@@ -23,8 +25,7 @@ class ArcusConan(ConanFile):
     exports = "LICENSE*"
     generators = "CMakeDeps", "VirtualBuildEnv", "VirtualRunEnv"
 
-    python_requires = "umbase/[>=0.1.7]@ultimaker/stable", "pyprojecttoolchain/[>=0.1.6]@ultimaker/stable", "sipbuildtool/[>=0.2.3]@ultimaker/stable"
-    python_requires_extend = "umbase.UMBaseConanfile"
+    python_requires = "pyprojecttoolchain/[>=0.1.6]@ultimaker/stable", "sipbuildtool/[>=0.2.3]@ultimaker/stable"
 
     options = {
         "shared": [True, False],
@@ -38,30 +39,55 @@ class ArcusConan(ConanFile):
         "py_build_requires": '"sip >=6, <7", "setuptools>=40.8.0", "wheel"',
         "py_build_backend": "sipbuild.api",
     }
-    scm = {
-        "type": "git",
-        "subfolder": ".",
-        "url": "auto",
-        "revision": "auto"
-    }
 
-    def set_version(self):
-        if self.version is None:
-            self.version = self._umdefault_version()
+    def _min_cppstd(self):
+        return 17
+
+    @property
+    def _compilers_minimum_version(self):
+        return {
+            "gcc": "9",
+            "clang": "9",
+            "apple-clang": "9",
+            "msvc": "192",
+            "visual_studio": "14",
+        }
+
+    def export_sources(self):
+        copy(self, "CMakeLists.txt", self.recipe_folder, self.export_sources_folder)
+        copy(self, "*", path.join(self.recipe_folder, "src"), path.join(self.export_sources_folder, "src"))
+        copy(self, "*", path.join(self.recipe_folder, "include"), path.join(self.export_sources_folder, "include"))
+        copy(self, "*", path.join(self.recipe_folder, "python"), path.join(self.export_sources_folder, "python"))
+
     def requirements(self):
-        self.requires("standardprojectsettings/[>=0.1.0]@ultimaker/stable")  # required for the CMake build modules
-        self.requires("sipbuildtool/0.2.3@ultimaker/stable")  # required for the CMake build modules
-        for req in self._um_data()["requirements"]:
-            self.requires(req)
+        self.requires("protobuf/3.21.9", transitive_headers=True)
+        self.requires("arcus/(latest)@ultimaker/cura_10475")  # TODO: point to `testing` once the CURA-10475 from libArcus is main
+        self.requires("cpython/3.10.4")  # Maybe place this in build_requirements as well
+        self.requires("zlib/1.2.12")
+
+    def validate(self):
+        if self.settings.compiler.cppstd:
+            check_min_cppstd(self, self._min_cppstd)
+        check_min_vs(self, 192)  # TODO: remove in Conan 2.0
+        if not is_msvc(self):
+            minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
+            if minimum_version and Version(self.settings.compiler.version) < minimum_version:
+                raise ConanInvalidConfiguration(
+                    f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support."
+                )
+
+    def build_requirements(self):
+        self.test_requires("standardprojectsettings/[>=0.1.0]@ultimaker/stable")
+        self.test_requires("sipbuildtool/0.2.3@ultimaker/stable")
 
     def config_options(self):
-        if self.options.shared and self.settings.compiler == "Visual Studio":
+        if self.settings.os == "Windows":
             del self.options.fPIC
 
     def configure(self):
-        self.options["protobuf"].shared = True
+        if self.options.shared:
+            self.options.rm_safe("fPIC")
         self.options["cpython"].shared = True
-        self.options["arcus"].shared = True
 
     def validate(self):
         if self.settings.compiler.get_safe("cppstd"):
@@ -78,6 +104,9 @@ class ArcusConan(ConanFile):
         pp.generate()
 
         tc = CMakeToolchain(self)
+        if is_msvc(self):
+            tc.variables["USE_MSVC_RUNTIME_LIBRARY_DLL"] = not is_msvc_static_runtime(self)
+        tc.cache_variables["CMAKE_POLICY_DEFAULT_CMP0077"] = "NEW"
         tc.variables["Python_EXECUTABLE"] = self.deps_user_info["cpython"].python.replace("\\", "/")
         tc.variables["Python_USE_STATIC_LIBS"] = not self.options["cpython"].shared
         tc.variables["Python_ROOT_DIR"] = self.deps_cpp_info["cpython"].rootpath.replace("\\", "/")
@@ -87,6 +116,9 @@ class ArcusConan(ConanFile):
         tc.variables["Python_FIND_STRATEGY"] = "LOCATION"
         tc.variables["Python_SITEARCH"] = "site-packages"
         tc.generate()
+
+        vb = VirtualBuildEnv(self)
+        vb.generate(scope="build")
 
         # Generate the Source code from SIP
         sip = self.python_requires["sipbuildtool"].module.SipBuildTool(self)
@@ -105,6 +137,7 @@ class ArcusConan(ConanFile):
         cmake.build()
 
     def package(self):
+        copy(self, pattern="LICENSE*", dst="licenses", src=self.source_folder)
         for ext in (".pyi", ".so", ".lib", ".a", ".pyd"):
             copy(self, f"pyArcus{ext}", self.build_folder, self.package_path.joinpath("lib"), keep_path = False)
 
@@ -113,8 +146,8 @@ class ArcusConan(ConanFile):
         copy(self, "*.h", self.source_path.joinpath("include"), self.package_path.joinpath("include"))
 
     def package_info(self):
-        self.cpp_info.libdirs = [ os.path.join(self.package_folder, "lib")]
+        self.cpp_info.libdirs = [ path.join(self.package_folder, "lib")]
         if self.in_local_cache:
-            self.runenv_info.append_path("PYTHONPATH", os.path.join(self.package_folder, "lib"))
+            self.runenv_info.append_path("PYTHONPATH", path.join(self.package_folder, "lib"))
         else:
             self.runenv_info.append_path("PYTHONPATH", self.build_folder)
