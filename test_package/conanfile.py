5e1e44e57bb1dbb075f1d9e0796c681ef4f5099b
Jelle Spijker <j.spijker@ultimaker.com>
2023-08-29 08:32:20 +0200

    invert run check
    contributes to CURA-10951

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -18,12 +18,12 @@ class ArcusTestConan(ConanFile):
         venv.generate()
 
     def build(self):
-        if not can_run(self):
+        if can_run(self):
             shutil.copy(Path(self.source_folder).joinpath("test.py"), Path(self.build_folder).joinpath("test.py"))
             shutil.copy(Path(self.source_folder).joinpath("test.proto"), Path(self.build_folder).joinpath("test.proto"))
 
     def test(self):
-        if not can_run(self):
+        if can_run(self):
             test_buf = StringIO()
             self.run(f"python test.py", env = "conanrun", output = test_buf, run_environment=True)
             ret_val = test_buf.getvalue()
